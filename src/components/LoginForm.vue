7ac7705a2480d2846680ea3d2c813a18ccfd7b5c
rijal <jeryzal333@gmail.com>
2022-07-17 15:44:35 +0700

    composable login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,22 +2,29 @@
   <form @submit.prevent="handleSubmit">
       <input type="email" required placeholder="email" v-model="email" >
       <input type="password" required placeholder="password" v-model="password" >
+      <p class="error">{{ error }}</p>
       <button>Sign up</button>
   </form>
 </template>
 
 <script>
+import userLogin from "../composable/userLogin"
 import { ref } from '@vue/reactivity'
+
 export default {
     setup() {
+        const { error, login } = userLogin()
         const email = ref("")
         const password = ref("")
 
-        const handleSubmit = () => {
-            console.log(email.value, password.value)
+        const handleSubmit = async () => {
+            await login(email.value, password.value)
+            if(!error.value) {
+                // console.log("User logged in")
+            }
         }
 
-        return { email, password, handleSubmit }
+        return { email, password, error, handleSubmit }
     }
 }
 </script>
---
+++ src/composable/userLogin.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+import { ref } from "vue";
+import { projectAuth } from "../firebase/firebaseApp";
+
+const error = ref(null);
+
+const login = async (email, password) => {
+  error.value = null;
+  try {
+    const res = await projectAuth.signInWithEmailAndPassword(email, password);
+    if (!res) {
+      throw new Error("Could not complete the signin");
+    }
+    error.value = null;
+    // await res.user.updateProfile({ displayName: displayName });
+    console.log(res);
+    return res;
+  } catch (err) {
+    error.value = err.message;
+  }
+};
+
+const useLogin = () => {
+  return { error, login };
+};
+
+export default useLogin;
