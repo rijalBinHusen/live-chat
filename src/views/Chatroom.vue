809dc443ae0401fb452d7c1eb342fd6cf3c43c9f
rijal <jeryzal333@gmail.com>
2022-07-17 20:19:54 +0700

    Navbar component

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,11 +1,14 @@
 <template>
-  <h3>Chatroom</h3>
+  <div class="container">
+    <Navbar />
+  </div>
 </template>
 
 <script>
+import Navbar from "../components/Navbar.vue"
 /* eslint-disable */
 export default {
-
+  components: { Navbar }
 }
 </script>
 
