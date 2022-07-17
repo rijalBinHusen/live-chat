685ec9d010976bb67dca9d655ecf77ae290e7409
rijal <jeryzal333@gmail.com>
2022-07-18 05:12:45 +0700

    watch user logout then redirect to login pages, i do that before in navbar component.

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -5,9 +5,21 @@
 </template>
 
 <script>
+import { useRouter } from 'vue-router'
 import Navbar from "../components/Navbar.vue"
+import getUser from '../composable/getUser'
 /* eslint-disable */
 export default {
+  setup() {
+    const { user } = getUser()
+    const router = useRouter()
+
+    watch((user), () => {
+      if(!user.value) {
+        router.push({ name: 'welcome' })
+      }
+    })
+  },
   components: { Navbar }
 }
 </script>
